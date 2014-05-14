f3772c8d6b7ba536e2a1fdd8a05deba6a063a9f0
maeponyo <maeponyo@gmail.com>
2014-05-14 11:34:54 +0900

    Create app2.js

---
+++ js/app2.js
@@ -0,0 +1,139 @@
+var INTERVAL = 1000;
+var DEFAULT_MESSAGE = "終了";
+
+var alarm = {
+		duration: -1,
+		message: ""
+};
+
+var formatCounterAsString = function(){
+		return "あと" + alarm.duration + "秒";
+};
+
+var updateCounter = function(){
+		alarm.output.textContent = formatCounterAsString();
+};
+
+var showAlarmMessage = function(){
+		var message = DEFAULT_MESSAGE;
+		if(alarm.message.length > 0){
+				message = alarm.message;
+		}
+		if(Notification.permission == "granted"){
+				var notification = new Notification(message);
+		}
+		alarm.output.textContent = message;
+};
+
+var update = function(){
+		alarm.duration = alarm.duration - 1;
+		if(isReadyToCountdown()){
+				updateCounter();
+				window.setTimeout(update, INTERVAL);
+		}else{
+				showAlarmMessage();
+		}
+};
+
+var isReadyToCountdown = function(){
+		return Number.isInteger(alarm.duration) && alarm.duration > 0;
+};
+
+var setupAlarm = function(durationString, message){
+		alarm.duration = Number(durationString),
+		alarm.message = message;
+};
+
+var startAlarm = function(){
+		setupAlarm(alarm.durationSelect.value, alarm.messageInput.value);
+		if(isReadyToCountdown()){
+				updateCounter();
+				window.setTimeout(update, INTERVAL);
+		}
+};
+
+var initApp = function(){
+		alarm.durationSelect = document.querySelector("#duration");
+		alarm.messageInput = document.querySelector("#message");
+		alarm.output = document.querySelector("#countdown");
+
+		Notification.requestPermission(function(status){
+				if(Notification.permission != status){
+						Notification.permission = status;
+				}
+		});
+
+		var startButton = document.querySelector("#start");
+		startButton.addEventListener("click", startAlarm);
+};
+
+initApp();var INTERVAL = 1000;
+var DEFAULT_MESSAGE = "終了";
+
+var alarm = {
+		duration: -1,
+		message: ""
+};
+
+var formatCounterAsString = function(){
+		return "あと" + alarm.duration + "秒";
+};
+
+var updateCounter = function(){
+		alarm.output.textContent = formatCounterAsString();
+};
+
+var showAlarmMessage = function(){
+		var message = DEFAULT_MESSAGE;
+		if(alarm.message.length > 0){
+				message = alarm.message;
+		}
+		if(Notification.permission == "granted"){
+				var notification = new Notification(message);
+		}
+		alarm.output.textContent = message;
+};
+
+var update = function(){
+		alarm.duration = alarm.duration - 1;
+		if(isReadyToCountdown()){
+				updateCounter();
+				window.setTimeout(update, INTERVAL);
+		}else{
+				showAlarmMessage();
+		}
+};
+
+var isReadyToCountdown = function(){
+		return Number.isInteger(alarm.duration) && alarm.duration > 0;
+};
+
+var setupAlarm = function(durationString, message){
+		alarm.duration = Number(durationString),
+		alarm.message = message;
+};
+
+var startAlarm = function(){
+		setupAlarm(alarm.durationSelect.value, alarm.messageInput.value);
+		if(isReadyToCountdown()){
+				updateCounter();
+				window.setTimeout(update, INTERVAL);
+		}
+};
+
+var initApp = function(){
+		alarm.durationSelect = document.querySelector("#honrei");
+		alarm.messageInput = document.querySelector("#yorei");
+		alarm.output = document.querySelector("#countdown");
+
+		Notification.requestPermission(function(status){
+				if(Notification.permission != status){
+						Notification.permission = status;
+				}
+		});
+
+		var startButton = document.querySelector("#start");
+		startButton.addEventListener("click", startAlarm);
+};
+
+initApp();
